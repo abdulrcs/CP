8bbb7074842a9d931e7e9c2085cc636b0d75aa21
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2020-09-12 17:31:17 +0800

    :tada: initial commit

---
+++ CSES/Missing_Number.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cmath>
+long long n;
+int main()
+{
+  scanf("%lld", &n);
+  int temp;
+  long long x = 0;
+  long long total = (n*(n+1))/2;
+  for(int i = 0;i<n-1;i++)
+  {
+    scanf("%i", &temp);
+    x+=temp;
+  }
+  printf("%lld\n", total-x);
+}
