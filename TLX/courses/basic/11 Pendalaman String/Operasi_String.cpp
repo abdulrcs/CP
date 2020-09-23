2c7990eb275e895e121fb73efb8229dfd1c732e7
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2020-09-23 16:44:26 +0800

    :tada: initial commit

---
+++ TLX/courses/basic/11 Pendalaman String/Operasi_String.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string s1,s2,s3,s4;
+int main()
+{
+  cin >> s1 >> s2 >> s3 >> s4;
+  s1.erase(s1.find(s2),s2.length());
+  s1.insert(s1.find(s3)+s3.length(),s4);
+  cout << s1 << endl;
+}
