aa120470acb526452f1105537fa3b2e9da135352
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2020-10-03 21:06:13 +0800

    :tada: initial commit

---
+++ CSES/increase.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+typedef long long ll;
+ll n,counter;
+int main()
+{
+  scanf("%lld", &n);
+  ll x[n];
+  for(int i = 0; i < n; i++)
+  {
+    scanf("%lld", &x[i]);
+    if(x[i-1] > x[i] && i != 0)
+    {
+      counter+=x[i-1]-x[i];
+      x[i]+=(x[i-1]-x[i]);
+    }
+  }
+  printf("%lld\n", counter);
+}
