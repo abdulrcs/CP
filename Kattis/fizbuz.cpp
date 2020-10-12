44436fbc94d930ccdbd123dfc38a6ebc83f1a14d
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2020-10-13 07:16:36 +0800

    :tada: initial commit

---
+++ Kattis/fizbuz.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int x,y,n;
+
+int main()
+{
+  cin >> x >> y >> n;
+  for(int i = 1; i <= n; i++)
+  {
+    if(i % x == 0 && i % y == 0)
+      cout << "FizzBuzz" << endl;
+    else if(i % x == 0)
+      cout << "Fizz" << endl;
+    else if(i % y == 0)
+      cout << "Buzz" << endl;
+    else
+      cout << i << endl;
+  }
+}
