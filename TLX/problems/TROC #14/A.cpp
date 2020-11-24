057e947f7d95074beb4945fbaa868dcde249b72e
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2020-11-24 17:11:50 +0800

    :tada: initial commit

---
+++ TLX/problems/TROC #14/A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define deb(x) cout << #x << "=" << x << endl
+
+int main()
+{
+  int m,n;
+  cin >> n >> m;
+  cout << n*m << endl;
+}
