a89d59bfaf4aa4f7b86865df233306386e535c94
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2020-12-26 18:29:09 +0800

    :tada: initial commit

---
+++ TLX/problems/IDEAFUSE 2014 Penyisihan CP/Website_Ideafuse.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s = "http://ideafuse.mikroskil.ac.id/";
+	string x; cin >> x;
+	cout << s+x << '/' << endl;
+}
